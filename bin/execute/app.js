8828d570bf86e988a1ad83cfe10fa14b72079e1a
Jonathan Perry <jonathanperry@modernpoacher.com>
2021-01-22 19:36:43 +0000

    1.0.42

--- bin/execute/app.js
+++ bin/execute/app.js
@@ -206,7 +206,7 @@ async function app () {
    *  `version` is printed into this file at pre-commit
    */
   commander
-    .version('1.0.41')
+    .version('1.0.42')
     .option('-p, --path [path]', 'Update path')
     .option('-f, --from [from]', 'Update from directory')
     .option('-o, --only [only]', 'Update only directory')

8df617a01b6908e45caa7f7080a585d988bc1429
Jonathan Perry <jonathanperry@modernpoacher.com>
2021-05-09 20:02:06 +0100

    1.0.44

--- bin/install/app.js
+++ bin/install/app.js
@@ -49,7 +49,7 @@ async function app () {
    *  `version` is printed into this file at pre-commit
    */
   commander
-    .version('1.0.43')
+    .version('1.0.44')
     .option('-P, --prod [dependencies]', 'Install `dependencies`', false)
     .option('-D, --dev [devDependencies]', 'Install `devDependencies`', false)
     .option('-O, --optional [optionalDependencies]', 'Install `optionalDependencies`', false)

e6ea5bba3ac9438d6159b20de21c79606d8e7600
Jonathan Perry <jonathanperry@modernpoacher.com>
2021-05-21 00:10:18 +0100

    `nvm`

--- src/common/index.js
+++ src/common/index.js
@@ -1,14 +1,42 @@
 import debug from 'debug'
 
-import {
-  readFileSync
-} from 'fs'
-
 const log = debug('@modernpoacher/deps')
 
 log('`common` is awake')
 
-export const NVM = readFileSync('./bin/common/nvm.sh').toString()
+export const NVM = `
+NVM=~/.nvm
+if [ -f "$NVM/nvm.sh" ];
+then
+  source $NVM/nvm.sh
+else
+  NVM=$(brew --prefix nvm)
+  if [ -f "$NVM/nvm.sh" ];
+  then
+    source $NVM/nvm.sh
+  fi
+fi
+
+VERSION=$(nvm --version)
+
+if [ -z "$VERSION" ];
+then
+  echo NVM not available
+else
+  echo NVM version $VERSION available
+
+  set -e
+
+  nvm use
+
+  if [[ $? != 0 ]];
+  then
+    echo NVM not configured
+  else
+    echo NVM configured
+  fi
+fi
+`
 
 /**
  *  @function getProdDependencies

3ba5f43f72774c5a92746ab3d7b4744c94106972
Jonathan Perry <jonathanperry@modernpoacher.com>
2021-05-22 17:28:25 +0100

    Updated `package.json` &/ `package-lock.json`

--- bin/install/app.js
+++ bin/install/app.js
@@ -49,7 +49,7 @@ async function app () {
    *  `version` is printed into this file at pre-commit
    */
   commander
-    .version('1.1.3')
+    .version('1.1.4')
     .option('-P, --prod [dependencies]', 'Install `dependencies`', false)
     .option('-D, --dev [devDependencies]', 'Install `devDependencies`', false)
     .option('-O, --optional [optionalDependencies]', 'Install `optionalDependencies`', false)

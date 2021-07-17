2d51d57bc1b457bee48792c84d283ab1fca7082b
Jonathan Perry <jonathanperry@modernpoacher.com>
2021-07-17 13:32:21 +0100

    `/dev/null`

--- src/common/git/index.js
+++ src/common/git/index.js
@@ -108,7 +108,7 @@ function isCommandError (e) {
 export function catGitRefsRemotesOriginHead (directory = DIRECTORY) {
   return (
     new Promise((resolve, reject) => {
-      const command = `EXP="[-0-9a-zA-Z]*$" && [[ $(cat "${directory}/.git/refs/remotes/origin/HEAD") =~ $EXP ]] && echo "\$\{BASH_REMATCH[0]\}"` // eslint-disable-line
+      const command = `[[ $(cat "${directory}/.git/refs/remotes/origin/HEAD") 2> /dev/null =~ "[-0-9a-zA-Z]*$" ]] && echo "$\{BASH_REMATCH[0]}"` // eslint-disable-line
 
       const {
         stdout,

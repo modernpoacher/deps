79c3db2f2da3a8cb9b6a8471e8d6b9eda918c07e
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-02-17 10:11:04 +0000

    `ssh-agent`

--- bin/deps.sh
+++ bin/deps.sh
@@ -6,10 +6,10 @@ export PATH=/usr/local/bin:$PATH
 
 # eval `ssh-agent`
 
-if [ $(ps ax | grep ssh-agent | wc -l) -eq 0 ] ; then
-  eval $(ssh-agent -s)
-  trap "ssh-agent -k" exit
-fi
+# if [ $(ps ax | grep ssh-agent | wc -l) -eq 0 ] ; then
+#   eval $(ssh-agent -s)
+#   trap "ssh-agent -k" exit
+# fi
 
 EXP="[-0-9a-zA-Z]*$"
 

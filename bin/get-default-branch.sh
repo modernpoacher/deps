5602d5b8d0d697e920d1b1c462198eb7993ddb06
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-02-20 00:49:14 +0000

    `ssh-agent`

--- bin/get-default-branch.sh
+++ bin/get-default-branch.sh
@@ -2,13 +2,6 @@
 
 export PATH=/usr/local/bin:$PATH
 
-# eval `ssh-agent`
-
-# if [ $(ps ax | grep ssh-agent | wc -l) -eq 0 ] ; then
-#   eval $(ssh-agent -s)
-#   trap "ssh-agent -k" exit
-# fi
-
 EXP="[-0-9a-zA-Z]*$"
 
 DIR=$(echo "$PWD" | sed 's/\\/\//g' | sed 's/://') && [[ $(cat "$DIR/.git/refs/remotes/origin/HEAD" 2> /dev/null) =~ $EXP ]] && echo "${BASH_REMATCH[0]}"
